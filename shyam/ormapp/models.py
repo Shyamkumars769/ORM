508ec531ba1dd3ee1840269a3fce0ed2a2b1101c
Shyamkumars769 <shyamsiva606@gmail.com>
2024-10-27 09:59:24 +0530

    Success

---
+++ shyam/ormapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib import admin
+class BankLoan(models.Model):
+    Name = models.CharField(max_length=100)
+    Account_No = models.IntegerField(primary_key="Account_No")
+    Phone_Number = models.IntegerField()
+    Aadhar_No = models.IntegerField()
+    Loan_amount = models.FloatField()
+    Time_Period = models.IntegerField()
+     
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Account_No', 'Phone_Number','Aadhar_No', 'Loan_amount', 'Time_Period',)
